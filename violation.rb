9fc72746c6287f8dc5f52c903c5d4e875a06f102
mintii <brittneynbraxton@gmail.com>
2016-07-14 23:54:34 -0500

    Add Date parsing

--- violation.rb
+++ violation.rb
@@ -1,13 +1,18 @@
+require 'date'
+
 class Violation
-	attr_reader :violation_id, :inspection_id, :violation_category, :violation_date, :violation_type
+	attr_reader :violation_id, :inspection_id, :violation_category, :violation_type
 	
 	def initialize(details)
 		@violation_id = details[:violation_id], 
 		@inspection_id = details[:inspection_id], 
 		@violation_category = details[:violation_category], 
-		@violation_date = details[:violation_date_closed], 
+		@violation_date = details[:violation_date], 
 		@violation_type = details[:violation_type]			
 	end
 
-	
+	def violation_date 
+		parsed_date_time = DateTime.parse(@violation_date)
+		parsed_date_time.strftime("%B %d, %Y")
+	end
 end
